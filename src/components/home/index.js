4c3995b337cbf3021cfd059386cd6beca863fb89
Austin Blain <austin.a.blain@gmail.com>
2018-01-21 21:19:20 -0800

    Section - Add date object, Add ternary for button render and class-assignment, Add styles for both and media-query

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
+import _ from "lodash";
 import Header from "./header";
 import Section from "./section";
 import Bio from "./bio";
 
 export default class Home extends Component {
-    // constructor() {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            date: {}
+        }
+    }
+
+    componentDidMount() {
+        const today = new Date();
+        const date = {
+            date: today,
+            day: today.getDay(),
+            hour: today.getHours(),
+            minute: today.getMinutes()
+        };
+        this.setState({ date });
+    }
 
     render() {
         return <div className="home">
             <Header />
             <div className="home-sections">
-                <Section title="Morning Appointments" subtitle="(9:00am - 12:00pm)" btnLink="" />
-                <Section title="Afternoon Appointments" subtitle="(1:00pm - 3:00pm)" btnLink="" />
-                <Section title="Evening Appointments" subtitle="(3:00pm - 6:00pm)" btnLink="" />
+              <Section date={this.state.date} period={1} title="Morning Appointments" subtitle="9:00am - 12:00pm" btnLink="" btnText="Booking open 8am - 12pm" />
+              <Section date={this.state.date} period={2} title="Afternoon Appointments" subtitle="1:00pm - 3:00pm" btnLink="" btnText="Booking open 12pm - 3:00pm" />
+              <Section date={this.state.date} period={3} title="Evening Appointments" subtitle="3:00pm - 6:00pm" btnLink="" btnText="Booking open 2pm - 6pm" />
             </div>
             <Bio />
           </div>;
